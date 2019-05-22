fa51d20e50327d2a79c1ca9f471632b2a7c84d74
Claudia <claudiamary.santos@gmail.com>
2019-05-22 19:14:47 -0300

    refatoracao codigo v2

--- js/script.js
+++ js/script.js
@@ -8,59 +8,40 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
         return response.json();
     })
     .then(data => {
-        // data.content.forEach(conteudo => {
-        //     console.log(conteudo)
-        //     const perfil = document.createElement('div');
-        //     perfil.setAttribute('class', 'maravilhosas__perfil')
-        //     const imagemPerfil = document.createElement('img');
-        //     imagemPerfil.setAttribute('class', 'img-responsive');
-        //     if (conteudo.metadata) {
-        //         if (conteudo.metadata.image) {
-        //             if (conteudo.metadata.image.url) {
-        //                 imagemPerfil.src = conteudo.metadata.image.url;                    }
-        //         }
-        //         else {
-        //             imagemPerfil.src = 'img/img-mulher.png'
-        //         }
-        //     } else {
-        //         imagemPerfil.src = 'img/img-mulher.png'
-        //     }
-
-
-        //     const namePerfil = document.createElement('p');
-        //     namePerfil.textContent = conteudo.title;
-
-        //     perfil.appendChild(imagemPerfil);
-        //     perfil.appendChild(namePerfil);
-
-        //     maravilhosasBox.appendChild(perfil);
-
-        // })
-        //})
-        for (let i = 0; i < data.content.length; i++) {
-            console.log(data.content)
+        data.content.forEach(conteudo => {
+            console.log(conteudo)
             const perfil = document.createElement('div');
             perfil.setAttribute('class', 'maravilhosas__perfil')
             const imagemPerfil = document.createElement('img');
             imagemPerfil.setAttribute('class', 'img-responsive');
-
-            if (data.content.metadata == undefined || data.content.metadata.image == "") {
+            // if (conteudo.metadata) {
+            //     if (conteudo.metadata.image) {
+            //         if (conteudo.metadata.image.url) {
+            //             imagemPerfil.src = conteudo.metadata.image.url;                    }
+            //     }
+            //     else {
+            //         imagemPerfil.src = 'img/img-mulher.png'
+            //     }
+            // } else {
+            //     imagemPerfil.src = 'img/img-mulher.png'
+            // }
+            if (conteudo.metadata == undefined || conteudo.metadata.image == "") {
                 imagemPerfil.src = 'img/img-mulher.png'
             }
             else {
-                imagemPerfil.src = data.content.metadata.image.url;
+                imagemPerfil.src = conteudo.metadata.image.url;
             }
 
 
             const namePerfil = document.createElement('p');
-            namePerfil.textContent = data.content.title;
+            namePerfil.textContent = conteudo.title;
 
             perfil.appendChild(imagemPerfil);
             perfil.appendChild(namePerfil);
 
             maravilhosasBox.appendChild(perfil);
-        }
 
+        })
     })
     .catch(erro => {
         console.log(erro)
